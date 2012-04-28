68a2028b0ee2db5e9b631203feade9ee0f691a8f
aqigmhqjh@gmail.com <aqigmhqjh@gmail.com@a1682eee-0a1a-bc38-5483-7e7acbed3e8b>
2012-04-28 07:33:15 +0000

    Add Weicheng Yang's code, but find too much similarity to Qi Wang's code; need more effort for examination!

---
+++ trunk/src/cn/edu/sjtu/se/weichengYang/detect/util/FindbugUtils.java
@@ -0,0 +1,30 @@
+package cn.edu.sjtu.se.weichengYang.detect.util;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.UnsupportedEncodingException;
+
+import edu.umd.cs.findbugs.Project;
+import edu.umd.cs.findbugs.ba.SourceFinder;
+import edu.umd.cs.findbugs.util.Util;
+
+public class FindbugUtils {
+	public static String getSourceFileContent(Project p,String packageName,String fileName){
+		SourceFinder sf = new SourceFinder(p);
+		BufferedReader rd;
+		try {
+			rd = new BufferedReader(Util.getReader(sf.findSourceFile(packageName,fileName).getInputStream()));
+			StringBuilder sb = new StringBuilder();
+			String line;
+			while((line=rd.readLine())!=null)
+				sb.append(line).append("\n");
+			return sb.toString();
+		} catch (UnsupportedEncodingException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		return null;
+	}
+	
+}
---
+++ trunk/src/cn/edu/sjtu/se/weichengYang/scala/util/ScalaType.java
@@ -0,0 +1,7 @@
+package cn.edu.sjtu.se.weichengYang.scala.util;
+
+import org.apache.bcel.generic.Type;
+
+public class ScalaType {
+	public static Type ANY = Type.OBJECT;
+}
